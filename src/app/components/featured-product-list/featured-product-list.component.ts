b1dcf3fb6f8900ddc84d5eaf4c66d3b01c3a21dd
Dusan Gojkovic <dusangojkovic087gmail.com>
2021-07-20 00:29:17 -0700

    slider featured products dodat

--- src/app/components/featured-product-list/featured-product-list.component.ts
+++ src/app/components/featured-product-list/featured-product-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ProductService } from 'src/app/Services/product.service';
 
 @Component({
@@ -6,14 +12,48 @@ import { ProductService } from 'src/app/Services/product.service';
   templateUrl: './featured-product-list.component.html',
   styleUrls: ['./featured-product-list.component.css'],
 })
-export class FeaturedProductListComponent implements OnInit {
+export class FeaturedProductListComponent implements OnInit,AfterViewInit {
   products?: any;
-
+  translateStartPosition:number = 200;
+  @ViewChild('slider') slider?:ElementRef;
+  sliderHTML?:HTMLElement;
+  sliderLength:number = 0;
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
       this.products = data;
+      this.sliderLength = this.products.length * 250;
     });
   }
+
+  ngAfterViewInit(){
+    this.sliderHTML = this.slider?.nativeElement as HTMLElement;
+  }
+
+  slideLeft(){
+    console.log(this.translateStartPosition);
+
+    if(this.sliderHTML){
+      if(this.translateStartPosition <= - this.sliderLength + 350){
+        this.translateStartPosition = 200;
+        this.sliderHTML.style.transform = `translateX(${this.translateStartPosition}px)`;
+      }
+      this.translateStartPosition -= 200;
+      this.sliderHTML.style.transform = `translateX(${this.translateStartPosition}px)`;
+    }
+
+  }
+
+  slideRight(){
+    if(this.sliderHTML){
+      if(this.translateStartPosition >= 0){
+        this.translateStartPosition = -200;
+        this.sliderHTML.style.transform = `translateX(${this.translateStartPosition}px)`;
+      }
+      this.translateStartPosition += 200;
+      this.sliderHTML.style.transform = `translateX(${this.translateStartPosition}px)`;
+    }
+  }
+
 }
