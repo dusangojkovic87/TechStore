f7ec6a8838209537d92e716cdbdc253ad3f37dec
Dusan Gojkovic <dusangojkovic087gmail.com>
2021-08-12 09:15:18 -0700

    contact page zavrsena

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BlogDetailsComponent } from './pages/blog/blog-details/blog-details.component';
 import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NewCollectionComponent } from './pages/new-collection/new-collection.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { SpecialsComponent } from './pages/specials/specials.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'home',component:HomeComponent},
+  {path:'new-collection',component:NewCollectionComponent},
+  {path:'specials',component:SpecialsComponent},
+  {path:'contact',component:ContactComponent},
   {path:'blog',component:BlogComponent},
   {path:'blog/:id',component:BlogDetailsComponent},
   {path:'product-details/:id',component:ProductDetailsComponent}
