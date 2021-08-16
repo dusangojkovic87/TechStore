70b3ecc18a0a8f4a6f6668a11cd92fb290db3f1f
Dusan Gojkovic <dusangojkovic087gmail.com>
2021-08-16 05:30:18 -0700

    special product stranica dodata

--- src/app/pages/specials/specials.component.ts
+++ src/app/pages/specials/specials.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Product } from 'src/app/Models/Product';
+import { ProductService } from 'src/app/Services/product.service';
 
 @Component({
   selector: 'app-specials',
   templateUrl: './specials.component.html',
-  styleUrls: ['./specials.component.css']
+  styleUrls: ['./specials.component.css'],
 })
 export class SpecialsComponent implements OnInit {
+  specialProducts: Product[] = [];
+  p:any = 1;
 
-  constructor() { }
+  constructor(private productServise: ProductService) {}
 
   ngOnInit(): void {
+    this.productServise
+      .getProducts()
+      .pipe(
+        map((products: any) => {
+          let filteredProducts = products.filter(
+            (product: Product) => product.specialProduct === true
+          );
+          return filteredProducts;
+        })
+      )
+      .subscribe((specialProducts) => {
+        if (specialProducts) this.specialProducts = specialProducts;
+      });
   }
-
 }
