de1a6fa24f7545ff27564d886a88785fd5651c39
Dusan Gojkovic <dusangojkovic087gmail.com>
2021-08-24 01:10:39 -0700

    responsive wishlist dodat

--- src/app/pages/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/wishlist.component.ts
@@ -7,21 +7,23 @@ import { ProductService } from 'src/app/Services/product.service';
   templateUrl: './wishlist.component.html',
   styleUrls: ['./wishlist.component.css'],
 })
-export class WishlistComponent implements OnInit,OnDestroy {
+export class WishlistComponent implements OnInit, OnDestroy {
   wishlist: any;
-  wishlistSubscription?:Subscription;
+  wishlistSubscription?: Subscription;
+  p: any = 1;
 
   constructor(private wishlistService: ProductService) {}
 
   ngOnInit(): void {
-      this.wishlistService.getWishlistProduct();
-      this.wishlistSubscription = this.wishlistService.wishlist$.subscribe(wishlist =>{
+    this.wishlistService.getWishlistProduct();
+    this.wishlistSubscription = this.wishlistService.wishlist$.subscribe(
+      (wishlist) => {
         this.wishlist = wishlist;
-
-      })
+      }
+    );
   }
 
-  ngOnDestroy(){
-   this.wishlistSubscription?.unsubscribe();
+  ngOnDestroy() {
+    this.wishlistSubscription?.unsubscribe();
   }
 }
